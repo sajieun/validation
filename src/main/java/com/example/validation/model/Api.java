4b6042a7cb82920248bb73de4f6a08fd751f76c7
sajieun <sajieun000721@naver.com>
2024-05-22 16:54:04 +0900

    error model create

---
+++ src/main/java/com/example/validation/model/Api.java
@@ -0,0 +1,31 @@
+package com.example.validation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Api<T> {
+
+    private String resultCode;
+
+    private String resultMessage;
+
+    private T data;
+
+    private Error error;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Error{
+        private List<String> errorMessage;
+    }
+}
