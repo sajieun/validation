caa3c7450de5a958b6e14c9ece2d26aee70aaa55
sajieun <sajieun000721@naver.com>
2024-05-22 17:43:27 +0900

    error message Response

--- src/main/java/com/example/validation/controller/UserApiController.java
+++ src/main/java/com/example/validation/controller/UserApiController.java
@@ -5,10 +5,13 @@ import com.example.validation.model.UserRegisterRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 import static org.springframework.http.codec.ServerSentEvent.builder;
 
@@ -18,12 +21,37 @@ import static org.springframework.http.codec.ServerSentEvent.builder;
 public class UserApiController {
 
     @PostMapping("")
-    public Api<UserRegisterRequest> register(
+    public Api<? extends Object> register(
             @Valid
             @RequestBody
-            Api<UserRegisterRequest> userRegisterRequest
+            Api<UserRegisterRequest> userRegisterRequest,
+
+            BindingResult bindingResult
     ){
         log.info("{}",userRegisterRequest);
+
+        if (bindingResult.hasErrors()){
+            var errorMessageList = bindingResult.getFieldErrors().stream()
+                    .map(it -> {
+                        var format = "%s : { %s } 은 %s";
+                        var massage = String.format(format, it.getField(), it.getRejectedValue(), it.getDefaultMessage());
+                        return massage;
+                    }).collect(Collectors.toList());
+
+            var error = Api.Error
+                    .builder()
+                    .errorMessage(errorMessageList)
+                    .build();
+
+            var errorResponese = Api
+                    .builder()
+                    .resultCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+                    .resultMessage(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .error(error)
+                    .build();
+
+            return errorResponese;
+        }
 
         var body = userRegisterRequest.getData();
         Api<UserRegisterRequest> response = Api.<UserRegisterRequest>builder()
--- src/main/java/com/example/validation/model/Api.java
+++ src/main/java/com/example/validation/model/Api.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Api<T> {
 
     private String resultCode;
